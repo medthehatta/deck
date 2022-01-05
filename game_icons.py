b3ba877a365bd475db373e357c4815659b568586
Med Mahmoud <medthehatta@gmail.com>
2022-01-05 14:42:21 +0000

    Add library for fetching game icons as SVGs

---
+++ game_icons.py
@@ -0,0 +1,19 @@
+from diskcache import Cache
+import requests
+
+from httprr import DefaultHandlers
+
+
+cache = Cache(__name__)
+
+
+# Cache for one week
+@cache.memoize(expire=3600*24*7)
+def svg(name):
+    if name.endswith(".svg"):
+        name = name.replace(".svg", "")
+    name = name.strip("/")
+    res = requests.get(
+        f"https://game-icons.net/icons/ffffff/000000/1x1/{name}.svg"
+    )
+    return DefaultHandlers.raise_or_return_text(res)

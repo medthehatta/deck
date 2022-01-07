41b2651055ff47b8ad56372d07d1214bda36d783
Med Mahmoud <medthehatta@gmail.com>
2022-01-07 13:54:11 +0000

    Distinguish rendering a deck from all decks in Generator

--- generator.py
+++ generator.py
@@ -22,7 +22,24 @@ class Generator:
     def relpath(self, path):
         return os.path.join(self.prefix, path)
 
-    def render(self, sheets_client):
+    def render_deck(self, sheets_client, func_name):
+        func_names = [func.__name__ for func in self.sheet_mapping()]
+        matches = [
+            (func, url) for (func, url) in self.sheet_mapping().items()
+            if func.__name__ == func_name
+        ]
+        if len(matches) == 0:
+            raise LookupError(f"No {func_name} in {func_names}")
+        elif len(matches) > 1:
+            raise LookupError(f"Ambiguous {func_name} in {func_names}")
+        else:
+            (func, url) = matches[0]
+            return [
+                func(x) for x in
+                sheets.entries(sheets_client, sheets.gsheet(url))
+            ]
+
+    def render_all(self, sheets_client):
         return {
             func.__name__: [
                 func(x) for x in

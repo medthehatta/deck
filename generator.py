8254894502c7d0c5794607400d49c1605ccbe506
Med Mahmoud <medthehatta@gmail.com>
2022-01-14 13:41:38 +0000

    Add layer of indirection and SvgDeck to generator

--- generator.py
+++ generator.py
@@ -10,15 +10,21 @@ class Deck:
     back: callable
     source: callable
 
+    def _face(self, record):
+        return self.face(record)
+
+    def _back(self, record):
+        return self.back(record)
+
     def render(self):
         records = self.source()
         return {
-            "faces": [self.face(record) for record in records],
-            "backs": [self.back(record) for record in records],
+            "faces": [self._face(record) for record in records],
+            "backs": [self._back(record) for record in records],
         }
 
     def render_faces(self):
-        return [self.face(record) for record in self.source()]
+        return [self._face(record) for record in self.source()]
 
     def render_backs(self):
-        return [self.back(record) for record in self.source()]
+        return [self._back(record) for record in self.source()]

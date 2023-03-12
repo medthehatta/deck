247ba40e2b251e40bbc4719579ea5f9035d96400
Med Mahmoud <medthehatta@gmail.com>
2023-03-12 11:54:00 +0000

    Bite the bullet and put SvgDeck and SvgTokens into deck

--- deck.py
+++ deck.py
@@ -2,6 +2,8 @@ import os
 from dataclasses import dataclass
 import sys
 
+from svg import svg_string_to_pil
+
 
 class Deck:
 
@@ -42,3 +44,46 @@ class Deck:
             return ["" for _ in records]
         else:
             return [self.name(record) for record in records]
+
+
+class Tokens:
+
+    def __init__(self, face, source, name=None):
+        self.face = face
+        self.source = source
+        self.name = name
+
+    def _face(self, record):
+        return self.face(record)
+
+    def preview_face(self):
+        return self._face(self.source()[0])
+
+    def preview_raw_face(self):
+        return self.face(self.source()[0])
+
+    def render(self):
+        records = self.source()
+        return [self._face(record) for record in records]
+
+    def names(self):
+        records = self.source()
+        if self.name is None:
+            return ["" for _ in records]
+        else:
+            return [self.name(record) for record in records]
+
+
+class SvgDeck(Deck):
+
+    def _face(self, record):
+        return svg_string_to_pil(self.face(record))
+
+    def _back(self, record):
+        return svg_string_to_pil(self.back(record))
+
+
+class SvgTokens(Tokens):
+
+    def _face(self, record):
+        return svg_string_to_pil(self.face(record))

550dfd8e234b1209032e8480d7163245377653eb
Med Mahmoud <medthehatta@gmail.com>
2023-03-12 12:16:56 +0000

    Add URL emitters to mancer_tts

--- mancer_tts.py
+++ mancer_tts.py
@@ -31,6 +31,12 @@ def tts_tokens(tokens):
     ])
 
 
+def tts_token_urls(tokens):
+    pils = tokens.render()
+    names = tokens.names()
+    return mancer_pils(pils)
+
+
 def tts_deck(deck):
     rendered = deck.render()
     names = deck.names()
@@ -42,6 +48,21 @@ def tts_deck(deck):
     front_url = mancer_pils([pils["faces"]])[0]
     back_url = mancer_pils([pils["backs"]])[0]
     return tts.deck(front_url, back_url, num_cards=num, card_names=names)
+
+
+def tts_deck_urls(deck):
+    rendered = deck.render()
+    num = len(rendered["faces"])
+    pils = tts.make_deck_pils(
+        face_pils=rendered["faces"],
+        back_pils=rendered["backs"],
+    )
+    front_url = mancer_pils([pils["faces"]])[0]
+    back_url = mancer_pils([pils["backs"]])[0]
+    return {
+        "faces": front_url,
+        "backs": back_url,
+    }
 
 
 def dump_tts_game(objects, path):

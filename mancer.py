b1c3ae3eaefb431f502a7a75ebcf3fe701096599
Med Mahmoud <medthehatta@gmail.com>
2023-06-13 10:04:03 +0000

    Add mancer_upload_pils

--- mancer.py
+++ mancer.py
@@ -1,6 +1,8 @@
 import os
 from io import BytesIO
 import hashlib
+import subprocess
+import tempfile
 
 
 def mancer_pils(pils):
@@ -13,6 +15,24 @@ def mancer_pils(pils):
         if not os.path.isfile(path):
             with open(path, "wb") as f:
                 pil.save(f)
+        urls.append(f"https://files.mancer.in/deck/{x}.png")
+
+    return urls
+
+
+def mancer_upload_pils(pils):
+    urls = []
+    for pil in pils:
+        buffered = BytesIO()
+        pil.save(buffered, format="PNG")
+        x = hashlib.sha1(buffered.getvalue()).hexdigest()
+        with tempfile.NamedTemporaryFile("wb") as f:
+            pil.save(f)
+            remote_path = f"/var/www/files/deck/{x}.png"
+            subprocess.check_call(
+                f"rsync {f.name} med@mancer.in:{remote_path}",
+                shell=True,
+            )
         urls.append(f"https://files.mancer.in/deck/{x}.png")
 
     return urls
